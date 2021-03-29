21ad18119e62d8618b7595f9776ff4d25ee79e9a
thienbinhwwww <phong@users.noreply.github.com>
2021-03-30 03:23:05 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function (req,res){
-    res.send('Hello');
+    res.send('Hello2');
 });
 
 app.listen(process.env.PORT||'3000');
