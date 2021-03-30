438bd6ccd2cf6bd18cfacbabe951e3475f7481ec
thienbinhwwww <phong@users.noreply.github.com>
2021-03-30 19:42:22 +0700

    a

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+
 var express = require('express');
+var expressHbs = require('express-handlebars')
 var app = express();
+app.listen(process.env.PORT || '3000');
+app.engine('handlebars', expressHbs({
+    layoutsDir: __dirname + '/views/layout',
+    // layout cha mặc định
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
-app.get('/',function (req,res){
-    res.send('Hello world');
-});
+app.get('/', function (request,
+                       response) {
+    console.log('ABABABABABABABAB')
+    response.render('index');
+})
 
-app.listen(process.env.PORT||'3000');
+app.get('/home', function (request, response) {
+    response.send('Homemmmm');
+})
