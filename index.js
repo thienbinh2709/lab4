85ce2997da9f5fc006cd32539b0fb32b57df599c
thienbinhwwww <phong@users.noreply.github.com>
2021-04-03 07:19:38 +0700

    a

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var express = require('express');
 var expressHbs = require('express-handlebars')
 var path = require('path');
 var app = express();
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.listen(process.env.PORT || '3001');
 app.engine('handlebars', expressHbs());
 app.set('view engine', 'handlebars');
 app.get('/', function (request,response) {
-
     response.render('tinde');
 })
 
